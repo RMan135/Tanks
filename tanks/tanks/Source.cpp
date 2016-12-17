d8c89e6b08be40c01658787eb38a55fd1775d356
RMan135 <r_man135@yahoo.com>
2016-12-17 16:29:29 +0200

    Added SLD library + test files

---
+++ tanks/tanks/Source.cpp
@@ -0,0 +1,89 @@
+#include <SDL.h>
+#include <iostream>
+using namespace std;
+
+#define SCREEN_WIDTH 640
+#define SCREEN_HEIGHT 480
+
+bool init();
+bool loadMedia();
+void close();
+
+SDL_Window* gWindow = NULL;
+SDL_Surface* gScreenSurface = NULL;
+SDL_Surface* gHelloWorld = NULL;
+
+int main(int argc, char* args[])
+{
+	if (!init())
+	{
+		cout << "Failed to initialize!" << endl;
+	}
+	else
+	{
+		if (!loadMedia())
+		{
+			cout << "Failed to load media!" << endl;
+		}
+		else
+		{
+			SDL_BlitSurface(gHelloWorld, NULL, gScreenSurface, NULL);
+			SDL_UpdateWindowSurface(gWindow);
+		}
+	}
+
+	SDL_Delay(2000);
+	close();
+	return 0;
+}
+
+bool init()
+{
+	bool success = true;
+
+	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+	{
+		cout << "SDL could not initialize!  SDL_Error: " << SDL_GetError() << endl;
+		success = false;
+	}
+	else
+	{
+		gWindow = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+
+		if (gWindow == NULL)
+		{
+			cout << "Window could not be created! SDL_Error: " << SDL_GetError() << endl;
+			success = false;
+		}
+		else
+		{
+			gScreenSurface = SDL_GetWindowSurface(gWindow);
+		}
+	}
+	return success;
+}
+
+bool loadMedia()
+{
+	bool success = true;
+
+	gHelloWorld = SDL_LoadBMP("media/splash.bmp");
+
+	if (gHelloWorld == NULL)
+	{
+		cout << "Unable to load image! SDL_Error: " << SDL_GetError() << endl;
+		success = false;
+	}
+	return success;
+}
+
+void close()
+{
+	SDL_FreeSurface(gHelloWorld);
+	gHelloWorld = NULL;
+
+	SDL_DestroyWindow(gWindow);
+	gWindow = NULL;
+
+	SDL_Quit();
+}
