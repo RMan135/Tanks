194846092bae256276bf6efb0ec6d7f01c5a38a3
RMan135 <r_man135@yahoo.com>
2016-12-27 21:57:25 +0200

    v0.01
    Not much to see here.

--- tanks/tanks/Source.cpp
+++ tanks/tanks/Source.cpp
@@ -3,7 +3,31 @@
 using namespace std;
 
 #define SCREEN_WIDTH 800
-#define SCREEN_HEIGHT 600
+#define SCREEN_HEIGHT 650
+#define FPS 60
+
+struct Sprite
+{
+	SDL_Surface *sprite;
+
+	void loadSprite(char path[])
+	{
+		sprite = SDL_LoadBMP(path);
+	}
+
+	void showSprite(SDL_Surface *dest, int x, int y)
+	{
+		SDL_Rect rect;
+		rect.x = x;
+		rect.y = y;
+		SDL_BlitSurface(sprite, NULL, dest, &rect);
+	}
+
+	void setAlpha(Uint32 color)
+	{
+		SDL_SetColorKey(sprite, SDL_TRUE, color);
+	}
+};
 
 int main(int argc, char* args[])
 {
@@ -24,14 +48,30 @@ int main(int argc, char* args[])
 		cout << "Could not get window surface! " << endl << SDL_GetError();
 	}
 
-	Uint32 blackColor = SDL_MapRGB(SCREEN->format, 0, 0, 0);
-	SDL_FillRect(SCREEN, NULL, blackColor);
-	SDL_UpdateWindowSurface(WINDOW);
+	Uint32 color_black = SDL_MapRGB(SCREEN->format, 0, 0, 0);
+	Uint32 startingTick;
 
+	///// TESTING GROUNDS /////
+	
+	Sprite s;
+	s.loadSprite("media/tank.bmp");
+	s.setAlpha(SDL_MapRGB(s.sprite->format, 255, 255, 255));
+	int x, y;
+	x = y = 0;
+
+	///////////////////////////
 	bool running = true;
 	SDL_Event event;
-	while(running)		//Main Loop
+
+	while(running)				// MAIN LOOP
 	{
+		// Get current tick (for fps cap)
+		startingTick = SDL_GetTicks();
+
+		// Make screen black
+		SDL_FillRect(SCREEN, NULL, color_black);
+
+		// Handle events
 		while (SDL_PollEvent(&event))
 		{
 			if (event.type == SDL_QUIT)
@@ -39,6 +79,21 @@ int main(int argc, char* args[])
 				running = false;
 				break;
 			}
+
+		}
+		//// TESTING GROUNDS ////
+
+		s.showSprite(SCREEN, (x++) % SCREEN_WIDTH, (y++) % SCREEN_HEIGHT);
+
+		/////////////////////////
+
+		// Update window
+		SDL_UpdateWindowSurface(WINDOW);
+
+		// Cap the fps
+		if ((1000 / FPS) > SDL_GetTicks() - startingTick)
+		{
+			SDL_Delay(1000 / FPS - (SDL_GetTicks() - startingTick));
 		}
 	}
 
