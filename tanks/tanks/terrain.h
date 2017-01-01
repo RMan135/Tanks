41b3be7069b322d1942ee121353c2167820ac3c5
C-Adrian <mihai.ciulin@info.uaic.ro>
2017-01-02 00:11:21 +0200

    Am adaugat preseturi, dar ceva se comporta ciudat
    Dupa citirea matricei collisionMap, elementele pot fi afisate corect, dar daca este verificat contentul lor printr-un if, sunt luate ca si 0. N-am nicio idee ce poate cauza asta.

--- tanks/tanks/terrain.h
+++ tanks/tanks/terrain.h
@@ -3,6 +3,10 @@
 
 #define MAX_TILES_WIDTH 32
 #define MAX_TILES_HEIGHT 24
+#define NUMBER_OF_PRESET_MAPS 4
+
+#include <fstream>
+ifstream fin;
 
 struct matrix
 {
@@ -12,7 +16,8 @@ struct matrix
 
 matrix collisionMap;
 matrix accessibleTiles;
-short theme;
+short mapTheme,
+      noOfPlayers;
 
 /*
 0 - empty
@@ -21,7 +26,7 @@ short theme;
 3 - spawn point nearby
 */
 
-/// mutati randurile 25-32 unde credeti ca e mai bine
+/// mutati randurile astea unde credeti ca e mai bine
 
 struct coordinates
 {
@@ -31,10 +36,47 @@ struct coordinates
 coordinates playerSpawners[4];
 coordinates powerupSpawners[4];
 
+///
+
 #include "terrainGeneration.h"
 
-void selectMap()
+void getMap()
 {
+    fin >> mapTheme
+        >> collisionMap.width
+        >> collisionMap.height;
+
+    for (int i = 0; i < 4; ++i)
+        fin >> playerSpawners[i].x
+            >> playerSpawners[i].y;
+
+    for (int i = 0; i < 4; ++i)
+        fin >> powerupSpawners[i].x
+            >> powerupSpawners[i].y;
+
+    for (int j = 0; j < collisionMap.height + 2; ++j)
+            for (int i = 0; i < collisionMap.width + 2; ++i)
+                fin >> collisionMap.tiles[i][j];
+
+}
+
+void selectMap(short choice)
+{
+    if (choice >= 0 && choice < NUMBER_OF_PRESET_MAPS)
+    {
+        switch (choice)
+        {
+            case 0 : { fin.open ("maps/forest1.dat");  break; }
+            case 1 : { fin.open ("maps/cave1.dat");    break; }
+            case 2 : { fin.open ("maps/ruins1.dat");   break; }
+            case 3 : { fin.open ("maps/forest1.dat");  break; }
+        }
+
+        getMap();
+        fin.close();
+    }
+
+    else
     generateMap();
 }
 
