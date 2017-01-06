1f8940b0380309cf1f1d20c98bd07334082c146d
RMan135 <r_man135@yahoo.com>
2017-01-06 16:07:38 +0200

    v0.02
    Added more complex render funciton.
    Some little changes.

--- tanks/tanks/Texture.cpp
+++ tanks/tanks/Texture.cpp
@@ -1,11 +1,13 @@
 #include "Texture.h"
-
+#include <iostream>
+using namespace std;
 Texture::Texture()
 {
 	texture = NULL;
 	renderTarget = NULL;
 	width = 0;
 	height = 0;
+	loaded = false;
 }
 
 Texture::~Texture()
@@ -21,14 +23,48 @@ void Texture::setRenderTarget(SDL_Renderer *newRenderTarget)
 
 void Texture::loadTexture(char path[])
 {
+	bool success = true;
 	SDL_Surface *image = IMG_Load(path);
+	if (image == NULL)
+	{
+		cout << "Could not load image from " << path << " ! " << SDL_GetError() << endl;
+		success = false;
+	}
 	texture = SDL_CreateTextureFromSurface(renderTarget, image);
-	width = image->w;
-	height = image->h;
+	if(texture == NULL)
+	{
+		cout << "Could not convert image from " << path << " ! " << SDL_GetError() << endl;
+		success = false;
+	}
+	
+	if (success)
+	{
+		loaded = true;
+		width = image->w;
+		height = image->h;
+	}
 }
 
 void Texture::simpleRender(int x, int y, double scale)
 {
+	if (!loaded)
+	{
+		cout << "Textrue has not been loaded yet!" << endl;
+	}
+	else
+	{
+		SDL_Rect renderSpace = { x, y, width*scale, height*scale };
+		SDL_RenderCopy(renderTarget, texture, NULL, &renderSpace);
+	}
+}
+
+void Texture::render(int x, int y, double angle, double scale, SDL_Rect* clip, SDL_RendererFlip flip, SDL_Point* center)
+{
 	SDL_Rect renderSpace = { x, y, width*scale, height*scale };
-	SDL_RenderCopy(renderTarget, texture, NULL, &renderSpace);
+	if (clip != NULL)
+	{
+		renderSpace.w = clip->w;
+		renderSpace.h = clip->h;
+	}
+	SDL_RenderCopyEx(renderTarget, texture, clip, &renderSpace, angle, center, flip);
 }
